8f559a3850e9efa12ed72abd30286af2eec0458c
Andrey Babenko <and.babenko96@gmail.com>
2023-06-01 12:42:44 +0300

    start

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Nuxt 3 test",
+      meta: [
+        {
+          name: "charset",
+          content: "utf-8",
+        },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: "description", content: "width=device-width, initial-scale=1" },
+      ],
+    },
+  },
+});
